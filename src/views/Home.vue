51a7e6e0b13d5692a230b172e737f573fe3f3ad2
Adam Suchi Hafizullah <adamsuchihafizullah@gmail.com>
2021-04-02 09:06:55 +0700

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis dolor quaerat eum voluptatem aperiam voluptatum temporibus, libero dicta commodi autem dolorem corporis quos ad asperiores, eligendi ratione minus magnam architecto dignissimos. Facilis corrupti facere id consequuntur officia magnam, repellat maxime, minus eos, qui alias non cumque libero! Eaque enim impedit omnis incidunt consequuntur perspiciatis labore hic repudiandae, ipsum laborum, animi accusantium dolores officiis id exercitationem molestias, culpa praesentium corrupti laudantium voluptate qui eius quis ut deleniti. At earum quam eius asperiores. Voluptates ullam assumenda soluta quae sapiente commodi est quia provident laudantium similique, non veritatis dolorem. Tenetur iure similique nulla!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis dolor quaerat eum voluptatem aperiam voluptatum temporibus, libero dicta commodi autem dolorem corporis quos ad asperiores, eligendi ratione minus magnam architecto dignissimos. Facilis corrupti facere id consequuntur officia magnam, repellat maxime, minus eos, qui alias non cumque libero! Eaque enim impedit omnis incidunt consequuntur perspiciatis labore hic repudiandae, ipsum laborum, animi accusantium dolores officiis id exercitationem molestias, culpa praesentium corrupti laudantium voluptate qui eius quis ut deleniti. At earum quam eius asperiores. Voluptates ullam assumenda soluta quae sapiente commodi est quia provident laudantium similique, non veritatis dolorem. Tenetur iure similique nulla!</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
